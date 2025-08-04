02d2f6b52b8353261b761637283055a59d9afa10
Bikszxc <rsotelo@usa.edu.ph>
2025-08-04 20:49:30 +0800

    added update action

--- src/Action.java
+++ src/Action.java
@@ -3,6 +3,7 @@ public class Action {
 
     private ActionType type;
     private Tasks task;
+    private Tasks oldTask;
     private int index;
 
     public Action(ActionType type, Tasks task) {
@@ -17,7 +18,15 @@ public class Action {
         this.index = index;
     }
 
+    public Action(ActionType type, Tasks oldTask, Tasks newTask) {
+        this.type = type;
+        this.oldTask = oldTask;
+        this.task = newTask;
+        this.index = -1;
+    }
+
     public int getIndex() {return index;}
     public ActionType getType() {return type;}
     public Tasks getTask() {return task;}
+    public Tasks getOldTask() {return oldTask;}
 }
